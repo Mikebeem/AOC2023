5ef99ce35260221f3231cbd11cf56a1116e5f2a5
Mike Beemsterboer <m.c.p.beemsterboer@dnb.nl>
2023-12-06 10:58:00 +0100

    :seedling: Day 5

--- Day5/1.ps1
+++ Day5/1.ps1
@@ -5,7 +5,6 @@ $inputVal = (ImportInput -Raw) -split '(?:\r?\n){2,}'
 $null, [bigint[]]$seeds = $inputVal[0].Split(" ")
 
 function getDestination ($list, $sourceVal) {
-    #$sourceVal = 53
     $destVal = $sourceVal
     foreach($line in $list){
         [bigint]$destination, [bigint]$source, [bigint]$length = $line.Split(" ")
@@ -20,7 +19,6 @@ function getDestination ($list, $sourceVal) {
 $LocationsList = @()
 foreach($seed in $seeds){
     $destVal = $seed
-    #$destval = 14
     foreach($inputList in $inputVal){
         $null, $list = $inputList -split '\n'
         $destVal = getDestination $list $destVal
@@ -30,29 +28,6 @@ foreach($seed in $seeds){
 }
 
 ($LocationsList | Measure-Object -Minimum).Minimum
-
-# $soilFert = $inputVal[2]
-# $fertWater = $inputList = $inputVal[3]
-# $waterLight = $inputVal[4]
-# $lightTemp = $inputVal[5]
-# $tempHum = $inputVal[6]
-# $humLocation = $inputVal[7]
-# $inputVal[8]
-
-
-# foreach($part in $inpuVal){
-#     $lines = ($part -split '\n')
-#     foreach($line in $lines){
-#         if($line.StartsWith('seeds')){
-            
-#         }
-#         elseif($line.StartsWith('seed')){
-
-#         }
-#     }
-    
-# }
-
 
 $stopwatch.Stop()
 write-host ("That took {0} Seconds. It took {1} in Milliseconds" -f $Stopwatch.Elapsed.TotalSeconds, $Stopwatch.Elapsed.TotalMilliseconds)
